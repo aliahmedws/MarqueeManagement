21f4a48025b19251f9798421c2a93ea3771c06d3
sherrykayani123 <kayanisheri123@gmail.com>
2026-02-17 18:41:11 +0500

    booking

---
+++ src/MarqueeManagement.Domain/BookingMenuItems/BookingMenuOptions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace MarqueeManagement.BookingMenuItems;
+
+public class BookingMenuOptions : FullAuditedAggregateRoot<Guid>
+{
+    public int Quantity { get; set; }
+    public decimal Price { get; set; }
+
+    private BookingMenuOptions()
+    {
+        //constructor 
+    }
+    internal BookingMenuOptions(Guid id, int quantity, decimal price)
+        : base(id)
+    {
+        Quantity = quantity;
+        Price = price;
+
+    }
+}
